072a5d3fb7c5dc3c457ad6a41ae72bb2e5a8c057
Khalid Laouhi <klaouhi@gmail.com>
2014-08-09 18:59:48 +0200

    Bugfixes and clean-up (see comments)
    - Homepage elements now correctly align vertically in the center
    - Error page (404) also vertically center aligned
    - Tweaked Fittext for some pages, they were breaking the layout on
    mobile (too wide/big text).
    - Fixed the header on Tokyo
    - Adjusted letter-spacing for Amsterdam
    - Cleaned up most of the comments in the CSS

--- site/snippets/footer-scripts.php
+++ site/snippets/footer-scripts.php
@@ -4,6 +4,9 @@
 		<script>
 			//jQuery(".inflate").fitText(0.8);
 			jQuery(".inflate").fitText(0.8, { minFontSize: '16px', maxFontSize: '288px' });
+			// jQuery(".sfo .inflate").fitText(1, { minFontSize: '16px', maxFontSize: '288px' });
+			// jQuery(".ams .inflate").fitText(0.9, { minFontSize: '16px', maxFontSize: '288px' });
+			// jQuery(".osl .inflate").fitText(0.4, { minFontSize: '16px', maxFontSize: '288px' });
 		</script>
 
 	<!-- ♫ -->
